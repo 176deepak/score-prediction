e0b5110a657dc54e80a3bfaf3e6106dc151f8ec3
Deepak <deepaksainiofficial17@gmail.com>
2023-06-16 21:42:46 +0530

    changes added

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,97 @@
- 
+import os
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.pipeline import Pipeline
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_object
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_filepath = os.path.join('artifacts', 'preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformation_object(self):
+        try:
+            numerical_features = ['writing_score', 'reading_score']
+            categorical_features = ['gender',
+                                    'race_ethnicity', 
+                                    'parental_level_of_education', 'lunch',
+                                    'test_preparation_course']
+            
+            numerical_pipeline = Pipeline(
+                steps=[('imputer', SimpleImputer(strategy='median')),
+                       ('scaler', StandardScaler())]
+            ) 
+
+            categorical_pipeline = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy='most_frequent')),
+                    ('one_hot_encoder', OneHotEncoder()),
+                    ('scaler', StandardScaler(with_mean=False))
+                ]
+            )
+            logging.info('Categorical columns encoding completed.')
+            logging.info('categorical columns encoding completed.')
+
+            preprocessor =  ColumnTransformer([
+                ('numerical_pipeline', numerical_pipeline,numerical_features),
+                ('categorical_pipeline', categorical_pipeline, categorical_features)
+            ])
+
+            return preprocessor
+         
+        except Exception as e: 
+            raise CustomException(e, sys)
+        
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path) 
+            test_df = pd.read_csv(test_path)
+
+            logging.info("Reading train and test data completed.")
+            logging.info('obtaining preprocessing object')
+            preprocessing_object = self.get_data_transformation_object()
+
+            target_column = 'math_score'
+
+            input_feature_train_df = train_df.drop(columns=[target_column], axis=1)
+            target_feature_train_df = train_df[target_column] 
+
+            input_feature_test_df = train_df.drop(columns=[target_column], axis=1)
+            target_feature_test_df = train_df[target_column] 
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+
+            input_feature_train_arr = preprocessing_object.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_object.transform(input_feature_test_df)
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
+
+            save_object(
+                filepath = self.data_transformation_config.preprocessor_filepath,
+                obj=preprocessing_object)
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_filepath
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
---
+++ src/utils.py
@@ -0,0 +1,56 @@
+import os 
+import sys
+import numpy as np
+import pandas as pd
+import dill
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.exception import CustomException
+
+def save_object(filepath, obj):
+    try:
+        dir_path = os.path.dirname(filepath)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(filepath, 'wb') as f:
+            pickle.dump(obj, f)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report:dict = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = params[list(models.keys())[i]] 
+
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predictions(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(filepath):
+    try:
+        with open(filepath, 'rb') as f:
+            return pickle.load(f)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
